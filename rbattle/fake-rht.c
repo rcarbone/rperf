4963a080927da05688397f8c85287a61625ab59f
rcarbone <rocco@tecsiel.it>
2017-07-11 19:34:52 +0200

    added file to rbattle

---
+++ rbattle/fake-rht.c
@@ -0,0 +1,76 @@
+/* System headers */
+#include <stdlib.h>
+
+
+/* librht - an abstract C library over real hash table implementations */
+typedef struct rht rht_t;
+#include "rht.h"
+
+
+typedef struct rht
+{
+  unsigned dummy;
+} rht_t;
+
+
+rht_t * rht_alloc (unsigned size)
+{
+  return (rht_t *) calloc (1, sizeof (rht_t));
+}
+
+
+void rht_free (rht_t * ht)
+{
+  if (ht)
+    free (ht);
+}
+
+
+void rht_clear (rht_t * ht)
+{
+}
+
+
+unsigned rht_count (rht_t * ht)
+{
+  return ht ? 1 : 0;
+}
+
+
+void rht_set (rht_t * ht, char * key, void * val)
+{
+}
+
+
+void * rht_get (rht_t * ht, char * key)
+{
+  return NULL;
+}
+
+
+void rht_del (rht_t * ht, char * key)
+{
+}
+
+
+bool rht_has (rht_t * ht, char * key)
+{
+  return false;
+}
+
+
+void rht_foreach (rht_t * ht, rht_each_f * fn, void * data)
+{
+}
+
+
+char ** rht_keys (rht_t * ht)
+{
+  return NULL;
+}
+
+
+void ** rht_vals (rht_t * ht)
+{
+  return NULL;
+}
