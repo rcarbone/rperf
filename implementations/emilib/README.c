fa7609ac533fa93eec05182e52ee43812b1dfc22
rcarbone <rocco@tecsiel.it>
2017-07-22 14:22:14 +0200

    added emilib implementation

--- implementations/emilib/README.c
+++ implementations/emilib/README.c
@@ -6,10 +6,10 @@ const char * variables   = "library module author description url";
 const char * functions   = "boot halt grow hit miss delete replace kbench";
 
 /* Plugin definitions */
-const char * library     = "tommyds";
-const char * module      = "tommyds/dynamic-ht";
-const char * author      = "Andrea Mazzoleni (amadvance@gmail.com)";
-const char * description = "Dynamic chained hashtable";
-const char * url         = "http://github.com/amadvance/tommyds";
+const char * library     = "emilib";
+const char * module      = "emilib::HashMap";
+const char * author      = "Emil Ernerfeldt (emil.ernerfeldt@gmail.com)";
+const char * description = "Loose collection of misc C++ libs";
+const char * url         = "https://github.com/emilk/emilib";
 
 /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
